1b2bd9d2e66d7d053a456108ee8b9bf662b89b19
caozhihui <caozhihui@4399inc.com>
2016-05-22 19:29:24 +0800

    充值活动

---
+++ wjdc/tool/gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var compass = require('gulp-compass');
+var minifycss = require('gulp-minify-css');
+
+
+
+
+
+
+gulp.task('scss',function(){
+    return gulp.src('../scss/*.scss')
+        .pipe(compass({
+            css:'../dev/css',
+            sass:'../dev/scss'
+
+        }))
+        .on('error',function(){
+            console.log('error');
+        })
+});
+
+gulp.task('css', function() {
+    return gulp.src('../dev/css/*.css')      //压缩的文件
+        .pipe(minifycss({
+            advanced:false,
+            //compatibility:'ie7',
+            keepBreaks:false
+        }))
+        .pipe(gulp.dest('../release/css/'));   //输出文件夹
+
+});
+
+
+gulp.task('auto',function(){
+    gulp.watch('../dev/scss/*.scss',['scss']);
+    gulp.watch('../dev/css/*.css',['css']);
+});
+
+
