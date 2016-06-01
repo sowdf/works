50b6e4774f2f373b0ef4e5d7d826a22e0030ffbf
caozhihui <caozhihui@4399inc.com>
2016-06-01 17:03:01 +0800

    开发到一半

--- wjdc/tool/gulpfile.js
+++ wjdc/tool/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-ruby-sass');
 var compass = require('gulp-compass');
 var minifycss = require('gulp-minify-css');
 
